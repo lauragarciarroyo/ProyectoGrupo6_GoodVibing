796527b0f5c1888cdfbe952e4407eb8f9c695264
Laura G. Arroyo <78605278+lauragarciarroyo@users.noreply.github.com>
2021-05-19 12:54:56 +0200

    Add files via upload

---
+++ 2.-BACK/repositories/index.js
@@ -0,0 +1,13 @@
+const imagesRepository = require('./imagesRepository');
+const commentsRepository = require('./commentsRepository');
+const storiesRepository = require('./storiesRepository');
+const usersRepository = require('./usersRepository');
+const votesRepository = require('./votesRepository');
+
+module.exports = {
+  imagesRepository,
+  commentsRepository,
+  storiesRepository,
+  usersRepository,
+  votesRepository
+};
---
+++ 2.-BACK/repositories/votesRepository.js
@@ -0,0 +1,17 @@
+const {database} = require ('../Infrastructure');
+
+async function addVotes() {
+    const query = 'INSERT INTO votes (stories_id user_id) VALUES (?, ?)';
+    const [result] = await database.pool.query(query, [storiesId, userId]);
+    return findStoriesById(result.insertId);
+
+}
+
+
+async function deleteVotes() {
+    const query = 'DELETE FROM votes WHERE id = ?';
+
+    return database.pool.query(query, id);
+}
+
+module.exports = {addVotes, deleteVotes}

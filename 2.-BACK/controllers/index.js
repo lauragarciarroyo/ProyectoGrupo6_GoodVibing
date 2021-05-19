8404e53bca3b0b7de7acb573e40a42a1bf977a2f
Laura G. Arroyo <78605278+lauragarciarroyo@users.noreply.github.com>
2021-05-19 12:52:16 +0200

    Add files via upload

---
+++ 2.-BACK/controllers/index.js
@@ -0,0 +1,13 @@
+const ImagesController = require('./imagesController');
+const CommentsController = require('./commentsController');
+const StoriesController = require('./storiesController');
+const UsersController = require('./usersController');
+const VotesController = require('./votesController');
+
+module.exports = {
+  ImagesController,
+  CommentsController,
+  StoriesController,
+  UsersController,
+  VotesController
+};

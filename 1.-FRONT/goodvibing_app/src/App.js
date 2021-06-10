c4e592fe6578a48d62a2419f44070412b5994575
Laura García Arroyo <lauragarciarroyo@gmail.com>
2021-06-10 10:18:55 +0200

    Front10junio
    Co-authored-by: beatriztc56 <beatriztc56@users.noreply.github.com>

--- 1.-FRONT/goodvibing_app/src/App.js
+++ 1.-FRONT/goodvibing_app/src/App.js
@@ -1,15 +1,32 @@
-import React, { useState } from "react";
-import Menu from "./Menu";
 import logo from "./goodvibing_logo_ps02.png";
-//import Login from "./Login";
 //import Register from "./Register";
 //import "./App.css";
 //import ErrorBoundary from "./ErrorBoundary";
+
+import Profile from "./Profile";
+import Stories from "./Stories";
+import Message from "./Message";
+import { Switch, Route } from "react-router-dom";
+import Menu from "./Menu";
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <main>
+        <Switch>
+          <Route path="/profile" exact>
+            <Profile />
+          </Route>
+          <Route path="/stories" exact>
+            <Stories />
+          </Route>
+          <Route path="/message" exact>
+            <Message />
+          </Route>
+        </Switch>
+      </main>
+      <p>Bienvenido</p>
     </div>
   );
 }
--- 1.-FRONT/goodvibing_app/src/Menu.js
+++ 1.-FRONT/goodvibing_app/src/Menu.js
@@ -1,4 +1,10 @@
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import Profile from "./Profile";
+import Stories from "./Stories";
+import Messages from "./Message";
+import Login from "./Login";
+//import "./Menu.css";
 
 function Menu() {
   const [open, setOpen] = useState(false);
@@ -9,11 +15,28 @@ function Menu() {
       {open && (
         <div className="modal-bg">
           <div className="modal-fg">
-            <li> Tus datos</li>
-            <li> Tus historias</li>
-            <li> Tus mensajes</li>
+            <li>
+              <NavLink to="/profile" activeClassName="active" exact>
+                <Profile />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/stories" activeClassName="active" exact>
+                <Stories />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/messages" activeClassName="active" exact>
+                <Messages />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/login" activeClassName="active" exact>
+                <Login />
+              </NavLink>
+            </li>
             <li> Cerrar sesión</li>
-            <button onClick={() => setOpen(false)}>Cerrar</button>
+            <button onClick={() => setOpen(false)}>X</button>
           </div>
         </div>
       )}

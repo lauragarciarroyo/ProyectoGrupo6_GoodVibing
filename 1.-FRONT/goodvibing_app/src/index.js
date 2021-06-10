c00ea6e9dfa5fb793582ff29626e302d07978397
beatriztc56 <beatriztc56gmail.com>
2021-06-10 22:35:54 +0200

    Imágenes

--- 1.-FRONT/goodvibing_app/src/index.js
+++ 1.-FRONT/goodvibing_app/src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Router } from "react-router";
 
 ReactDOM.render(
   <React.StrictMode>

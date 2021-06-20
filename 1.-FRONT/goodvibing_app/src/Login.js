ecd7f4f83813ef76918d3eba69bbefd890f5b192
beatriztc56 <beatriztc56gmail.com>
2021-06-20 18:01:37 +0200

    Login

--- 1.-FRONT/goodvibing_app/src/Login.js
+++ 1.-FRONT/goodvibing_app/src/Login.js
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -22,34 +31,111 @@ function Login() {
       dispatch({ type: "SET_ERROR", message: data.message });
     }
   };
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: "100%", // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+  const classes = useStyles();
+  function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {"Copyright © "}
+        <Link color="inherit" href="https://material-ui.com/">
+          GoodVibing
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
 
   if (isLoggedIn) {
     return <Redirect to="/" />;
   }
   return (
-    <form className="Login" onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input
-          placeholder="Email..."
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+    <Container component="main" maxWidth="xs">
+      <form className="Login" onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            placeholder="Email..."
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Correo electrónico"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Contraseña"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <p />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Recuérdame"
         />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type="password"
-          placeholder="Password..."
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <br />
-      <button>Entrar</button>
-      <button>Seguir como invitado</button>
-    </form>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+        >
+          ¡Entra!
+        </Button>
+        <Grid container>
+          <Grid item xs>
+            <Link href="#" variant="body2">
+              ¿Olvidaste tu contraseña?
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link href="./SignUp" variant="body2">
+              {"¿Aún no tienes cuenta? ¡Regístrate!"}
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
 

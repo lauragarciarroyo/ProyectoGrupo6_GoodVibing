e17a76811c7038ebf6bab3e2fac8ad749742debc
Laura G. Arroyo <78605278+lauragarciarroyo@users.noreply.github.com>
2021-06-21 19:17:48 +0200

    Add files via upload

---
+++ 1.-FRONT/goodvibing_app/src/App.js
@@ -0,0 +1,163 @@
+import React from "react";
+import "./App.css";
+import Profile from "./Profile";
+import Message from "./Message";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Menu from "./Menu";
+import Home from "./Home";
+import Contact from "./Contact";
+import CreateStory from "./CreateStory";
+import MyStories from "./MyStories";
+import DeleteUser from "./Deteleuser";
+import Changepassword from "./ChangePassword";
+import RandomStories from "./RandomStories";
+import MyStory from "./MyStory";
+import EditStory from "./EditStory";
+import DeleteStory from "./DeleteStory";
+import CreateComment from "./CreateComment";
+import ErrorMesage from "./components/ErrorMesage";
+import ViewStory from "./ViewStory";
+import { useDispatch, useSelector } from "react-redux";
+import ViewStories from "./ViewStories";
+import MyComments from "./MyComments";
+import Userinfo from "./Userinfo";
+import CreateVote from "./CreateVote";
+import "fontsource-roboto";
+import Register from "./Register";
+import Login from "./Login";
+import Blog from "./Blog";
+import FeaturedPost from "./FeaturedPost";
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+import MainFeaturedPost from "./MainFeaturedPost";
+import Markdown from "./Markdown";
+import Sidebar from "./Sidebar";
+
+const PrivateRoute = ({ children }) => {
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  if (!user) {
+    dispatch({ type: "SET_ERROR", message: "Esta página es privada" });
+    return <Redirect to="/" />;
+  } else {
+    return children;
+  }
+};
+
+function App() {
+  return (
+    <div className="App">
+      <ErrorMesage />
+      <Menu />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/mystories" exact>
+            <PrivateRoute>
+              <MyStories />
+            </PrivateRoute>
+          </Route>
+          <Route path="/message" exact>
+            <PrivateRoute>
+              <Message />
+            </PrivateRoute>
+          </Route>
+          <Route path="/login" exact>
+            <Login />
+          </Route>
+          <Route path="/register" exact>
+            <Register />
+          </Route>
+          <Route path="/profile" exact>
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          </Route>
+          <Route path="/contact" exact>
+            <Contact />
+          </Route>
+          <Route path="/createstory" exact>
+            <PrivateRoute>
+              <CreateStory />
+            </PrivateRoute>
+          </Route>
+          <Route path="/deleteuser" exact>
+            <PrivateRoute>
+              <DeleteUser />
+            </PrivateRoute>
+          </Route>
+          <Route path="/changepassword" exact>
+            <Changepassword />
+          </Route>
+          <Route path="/viewstories/:id" exact>
+            <ViewStories />
+          </Route>
+          <Route path="/randomstories/" exact>
+            <RandomStories />
+          </Route>
+          <Route path="/mystory/:id" exact>
+            <PrivateRoute>
+              <MyStory />
+            </PrivateRoute>
+          </Route>
+          <Route path="/editstory/:id" exact>
+            <PrivateRoute>
+              <EditStory />
+            </PrivateRoute>
+          </Route>
+          <Route path="/deletestory/:id" exact>
+            <PrivateRoute>
+              <DeleteStory />
+            </PrivateRoute>
+          </Route>
+          <Route path="/createcomment" exact>
+            <CreateComment />
+          </Route>
+          <Route path="/story/:id" exact>
+            <PrivateRoute>
+              <ViewStory />
+            </PrivateRoute>
+          </Route>
+          <Route path="/mycomments" exact>
+            <MyComments />
+          </Route>
+          <Route path="/userinfo" exact>
+            <Userinfo />
+          </Route>
+          <Route path="/createvote" exact>
+            <CreateVote />
+          </Route>
+          <Route path="/blog" exact>
+            <Blog />
+          </Route>
+          <Route path="/featuredpost" exact>
+            <FeaturedPost />
+          </Route>
+          <Route path="/footer" exact>
+            <Footer />
+          </Route>
+          <Route path="/header" exact>
+            <Header />
+          </Route>
+          <Route path="/main" exact>
+            <Main />
+          </Route>
+          <Route path="/mainfeaturedpost" exact>
+            <MainFeaturedPost />
+          </Route>
+          <Route path="/markdown" exact>
+            <Markdown />
+          </Route>
+          <Route path="/sidebar" exact>
+            <Sidebar />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;

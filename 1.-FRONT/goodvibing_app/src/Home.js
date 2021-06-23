ffb6c97509930ac7d40867c6ce4fdc3846fc868a
Laura García Arroyo <lauragarciarroyo@gmail.com>
2021-06-23 19:29:23 +0200

    patatas

--- 1.-FRONT/goodvibing_app/src/Home.js
+++ 1.-FRONT/goodvibing_app/src/Home.js
@@ -1,14 +1,13 @@
 import React from "react";
-import StoriesHome from "./StoriesHome";
-import goodvibing_logo_ps01 from "./assets/img/goodvibing_logo_ps01.jpg";
+import StoriesHome from "./StoriesHome2";
 import "./Home.css";
 
 export default function Home() {
   return (
     <div>
-      <h1>Bienvenido a Goodvibing</h1>
+      <h1>¡ Bienvenido a Goodvibing !</h1>
       <h2>
-        La plataforma donde podrás compartir <u>SOLO</u> - <u>BUENAS</u> -{" "}
+        La plataforma donde podrás compartir <u>SOLO</u> - <u>BUENAS</u> -
         <u>NOTICIAS</u>
       </h2>
       <StoriesHome />
--- 1.-FRONT/goodvibing_app/src/StoriesHome.js
+++ 1.-FRONT/goodvibing_app/src/StoriesHome.js
@@ -1,45 +1,110 @@
-import { Button, Container, makeStyles } from "@material-ui/core";
-import useFetch from "./UseFetch";
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
 
-function StoriesHome() {
-  const results = useFetch(`http://localhost:4000/api/stories`);
-
-  const useStyles = makeStyles((theme) => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      width: "100%",
-    },
-  }));
-  const classes = useStyles();
-
-  if (!results) return <p>Cargando...</p>;
-
+function Copyright() {
   return (
-    <div className={classes.paper}>
-      <div className="Historias">
-        {results.data.map((e) => (
-          <div key={e.id}>
-            <Button
-              href={`/story/${e.id}`}
-              color="primary"
-              style={{ margin: 10 }}
-            >
-              {e.title}
-            </Button>
-            <Container style={{ margin: 10 }}>
-              <p>{e.body}</p>
-            </Container>
-          </div>
-        ))}
-        {!results.data.length && <i>No se han encontrado historias</i>}
-      </div>
-    </div>
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="/">
+        GoodVibing
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
   );
 }
 
-export default StoriesHome;
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
 
-//Solo hay que mostrar 3 historias aleatorias
+const cards = [1, 2, 3];
+
+export default function Album() {
+  const classes = useStyles();
+
+  return (
+           <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe
+                      the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+       <footer className={classes.footer}>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          ¡Comparte tus buenas historias!
+        </Typography>
+        <Copyright />
+      </footer>
+      </React.Fragment>
+  );
+}

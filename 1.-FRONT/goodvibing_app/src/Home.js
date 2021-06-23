e6a88631e7ef657823267bf32318b4e8b039c707
Laura García Arroyo <lauragarciarroyo@gmail.com>
2021-06-23 16:03:43 +0200

    DiseñoHomeV1

--- 1.-FRONT/goodvibing_app/src/Home.js
+++ 1.-FRONT/goodvibing_app/src/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
 import StoriesHome from "./StoriesHome";
+import logoEntero_blanco from "./assets/img/logoEntero_blanco.png";
 
 export default function Home() {
   return (
     <div>
-      <h1>Portada</h1>
+      <h1>
+        <p>Bienvenido a </p>
+        <p>
+          <img src={logoEntero_blanco} alt="logo" width="200"></img>
+        </p>
+      </h1>
+      <h3>La plataforma donde podrás compartir SOLO-BUENAS-NOTICIAS</h3>
       <StoriesHome />
     </div>
   );

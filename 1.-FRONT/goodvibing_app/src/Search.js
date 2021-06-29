76c7f09604cf0b0b4ffcdc3e9cac38972922e701
beatriztc56 <beatriztc56gmail.com>
2021-06-29 19:40:10 +0200

    Search

--- 1.-FRONT/goodvibing_app/src/Search.js
+++ 1.-FRONT/goodvibing_app/src/Search.js
@@ -1,28 +1,52 @@
-import { Link, useLocation } from "react-router-dom";
+import { Box, Button, makeStyles } from "@material-ui/core";
+import { useLocation } from "react-router-dom";
 import RandomStories from "./RandomStories";
 import useFetch from "./UseFetch";
 
 function Search({ q }) {
   const stories = useFetch(`http://localhost:4000/api/stories?q=${q}`);
 
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      display: "flex",
+      flexGrow: 1,
+      flexDirection: "column",
+      alignItems: "center",
+      width: "100%",
+    },
+  }));
+  const classes = useStyles();
   if (!stories) return <p>Cargando...</p>;
 
   return (
     <div className="search">
-      {stories.data.length > 0 ? (
-        <div className="history">
-          <h2>Historias</h2>
-          <ul>
-            {stories.data.map((s) => (
-              <li key={s.id}>
-                <Link to={`/story/${s.id}`}>{s.title}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <p>No hay resultados...</p>
-      )}
+      <div className={classes.paper}>
+        {stories.data.length > 0 ? (
+          <div className="history">
+            <h2>Historias</h2>
+            <ul>
+              {stories.data.map((s) => (
+                <div key={s.id}>
+                  <Box
+                    borderRadius={16}
+                    justifyContent="center"
+                    alignItems="center"
+                    boxShadow={20}
+                    p={2}
+                    m={2}
+                    display="block"
+                    css={{ maxWidth: 600 }}
+                  >
+                    <Button href={`/story/${s.id}`}>{s.title}</Button>
+                  </Box>
+                </div>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>No hay resultados...</p>
+        )}
+      </div>
     </div>
   );
 }

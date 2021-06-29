961899399d5c79f3229b15993b89f2a9bb38a6c5
beatriztc56 <beatriztc56gmail.com>
2021-06-29 14:23:18 +0200

    front

--- 1.-FRONT/goodvibing_app/src/StoriesHome.js
+++ 1.-FRONT/goodvibing_app/src/StoriesHome.js
@@ -1,46 +1,60 @@
-import { Button } from "@material-ui/core";
+import { Button, Grid, makeStyles, Paper } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
+import React from "react";
 import useFetch from "./UseFetch";
 
 function StoriesHome() {
   const results = useFetch(`http://localhost:4000/api/storieshome`);
+
   const defaultProps = {
     bgcolor: "background.paper",
     m: 1,
-    style: { width: "30rem", height: "15rem" },
+    style: { width: "50rem", height: "25rem" },
     borderColor: "text.primary",
   };
+
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      height: 50,
+      width: 300,
+    },
+  }));
+  const classes = useStyles();
 
   if (!results) return <p>Cargando...</p>;
 
   return (
     <div className="Historias">
-      {results.data.map((e) => (
-        <Box
-          justifyContent="center"
-          alignItems="center"
-          mx="auto"
-          border={1}
-          boxShadow={3}
-          bgcolor="background.paper"
-          display="block"
-          css={{ maxWidth: 800 }}
-          {...defaultProps}
-          p={2}
-          key={e.id}
-        >
-          <Button href={`/story/${e.id}`} fullWidth color="primary">
-            {e.title}
-          </Button>
-
-          <p>{e.body}</p>
-        </Box>
-      ))}
-      {!results.data.length && <i>No se han encontrado historias</i>}
+      <Grid item xs={16}>
+        <Grid container spacing={1}>
+          {results.data.map((e) => (
+            <Box
+              justifyContent="center"
+              alignItems="center"
+              mx="auto"
+              border={1}
+              boxShadow={3}
+              bgcolor="background.paper"
+              display="block"
+              css={{ maxWidth: 400 }}
+              {...defaultProps}
+              p={3}
+            >
+              <Paper className={classes.paper}>
+                <div key={e.id}>
+                  <Button href={`/story/${e.id}`} color="primary">
+                    {e.title}
+                  </Button>
+                  <p>{e.body}</p>
+                </div>
+              </Paper>
+            </Box>
+          ))}
+          {!results.data.length && <i>No se han encontrado historias</i>}
+        </Grid>
+      </Grid>
     </div>
   );
 }
 
 export default StoriesHome;
-
-//Solo hay que mostrar 3 historias aleatorias

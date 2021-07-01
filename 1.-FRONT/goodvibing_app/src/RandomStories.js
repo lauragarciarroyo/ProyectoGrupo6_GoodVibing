a168944dc22bbf4fd90573c849bdedea282e4542
beatriztc56 <beatriztc56gmail.com>
2021-07-02 00:01:32 +0200

    search

--- 1.-FRONT/goodvibing_app/src/RandomStories.js
+++ 1.-FRONT/goodvibing_app/src/RandomStories.js
@@ -1,58 +1,20 @@
-import { Box, Button, makeStyles } from "@material-ui/core";
 import UseFetch from "./UseFetch";
-import { Typography } from "@material-ui/core";
 import StoryTitle from "./StoryTitle";
 
 function RandomStories({ q }) {
   const stories = UseFetch(`http://localhost:4000/api/random-stories`);
-
-  const useStyles = makeStyles((theme) => ({
-    paper: {
-      display: "flex",
-      flexGrow: 1,
-      flexDirection: "column",
-      alignItems: "center",
-      width: "100%",
-    },
-  }));
-  const classes = useStyles();
 
   if (!stories) return <p>Cargando...</p>;
 
   return (
     <div className="randomstories">
       <h1>Explora historias aleatorias</h1>
-      <div className={classes.paper}>
-        <div>
-          {stories.data.map((s) => (
-            <div key={s.id}>
-              <Box
-                borderRadius={16}
-                justifyContent="center"
-                alignItems="center"
-                boxShadow={20}
-                p={2}
-                m={2}
-                display="block"
-                css={{ maxWidth: 600 }}
-              >
-                <StoryTitle color="primary" href={`/story/${s.id}`}>
-                  {s.title}
-                </StoryTitle>
-              </Box>
-            </div>
-          ))}
-        </div>
-      </div>
       <div>
-        <Typography variant="body2" color="textSecondary" align="center">
-          {"Copyright © "}
-          <Button color="inherit" href="/">
-            GoodVibing
-          </Button>{" "}
-          {new Date().getFullYear()}
-          {"."}
-        </Typography>
+        {stories.data.map((s) => (
+          <div key={s.id}>
+            <StoryTitle href={`/story/${s.id}`}>{s.title}</StoryTitle>
+          </div>
+        ))}
       </div>
     </div>
   );
--- 1.-FRONT/goodvibing_app/src/Search.js
+++ 1.-FRONT/goodvibing_app/src/Search.js
@@ -1,4 +1,3 @@
-import { Box, makeStyles } from "@material-ui/core";
 import { useLocation } from "react-router-dom";
 import RandomStories from "./RandomStories";
 import useFetch from "./UseFetch";
@@ -7,47 +6,24 @@ import StoryTitle from "./StoryTitle";
 function Search({ q }) {
   const stories = useFetch(`http://localhost:4000/api/stories?q=${q}`);
 
-  const useStyles = makeStyles((theme) => ({
-    paper: {
-      display: "flex",
-      flexGrow: 1,
-      flexDirection: "column",
-      alignItems: "center",
-      width: "100%",
-    },
-  }));
-  const classes = useStyles();
   if (!stories) return <p>Cargando...</p>;
 
   return (
     <div className="search">
-      <div className={classes.paper}>
-        {stories.data.length > 0 ? (
-          <div className="history">
-            <h2>Historias</h2>
-            <ul>
-              {stories.data.map((s) => (
-                <div key={s.id}>
-                  <Box
-                    borderRadius={16}
-                    justifyContent="center"
-                    alignItems="center"
-                    boxShadow={20}
-                    p={2}
-                    m={2}
-                    display="block"
-                    css={{ maxWidth: 600 }}
-                  >
-                    <StoryTitle href={`/story/${s.id}`}>{s.title}</StoryTitle>
-                  </Box>
-                </div>
-              ))}
-            </ul>
-          </div>
-        ) : (
-          <p>No hay resultados...</p>
-        )}
-      </div>
+      {stories.data.length > 0 ? (
+        <div className="history">
+          <h2>Historias</h2>
+          <ul>
+            {stories.data.map((s) => (
+              <div key={s.id}>
+                <StoryTitle href={`/story/${s.id}`}>{s.title}</StoryTitle>
+              </div>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>No hay resultados...</p>
+      )}
     </div>
   );
 }

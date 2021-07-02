20efedfc0f89c6600085def17cd9b97d4da56012
beatriztc56 <beatriztc56gmail.com>
2021-07-02 13:38:53 +0200

    createstory

--- 1.-FRONT/goodvibing_app/src/CreateStory.js
+++ 1.-FRONT/goodvibing_app/src/CreateStory.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "./Header";
-import { Container, CssBaseline, TextField } from "@material-ui/core";
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
@@ -34,50 +33,35 @@ function CreateStory() {
   };
 
   return (
-    <Container component="main" maxWidth="sm">
-      <CssBaseline />
+    <div className="createstory">
       <Header title="¡Bienvenid@ a tu blog!" />
-      <div className="historias">
-        <div className="preview">
-          <h1>{title}</h1>
-          <p>{body}</p>
-        </div>
-        <form className="edituser" onSubmit={handleSubmit}>
-          <label>
-            <TextField
-              id="filled-basic"
-              placeholder="Escribe aquí el título..."
-              value={title}
-              margin="normal"
-              onChange={(e) => setTitle(e.target.value)}
-              maxWidth="xs"
-              required
-              rowsMax="1"
-              size="medium"
-              style={{ margin: 40 }}
-              align="center"
-            />
-          </label>
-          <p />
-          <label>
-            <TextField
-              id="filled-basic"
-              variant="filled"
-              multiline={true}
-              value={body}
-              onChange={(e) => setBody(e.target.value)}
-              style={{ margin: 50 }}
-              required
-              textareautosize
-              placeholder="Escribe aquí tu historia..."
-            />
-          </label>
-          <p />
-
-          <button>Guardar historia</button>
-        </form>
+      <div className="preview">
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
-    </Container>
+      <form className="create" onSubmit={handleSubmit}>
+        <label>
+          <input
+            className="createtitle"
+            placeholder="Escribe aquí el título..."
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+        <p />
+        <label>
+          <input
+            className="createbody"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+            placeholder="Escribe aquí tu historia..."
+          />
+        </label>
+        <p />
+
+        <button>Guardar historia</button>
+      </form>
+    </div>
   );
 }
 

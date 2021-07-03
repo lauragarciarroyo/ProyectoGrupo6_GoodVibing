d1455ecfdb446f521053cec7e22f30b452c15705
beatriztc56 <beatriztc56gmail.com>
2021-07-03 17:16:02 +0200

    mas cambios

--- 1.-FRONT/goodvibing_app/src/EditStory.js
+++ 1.-FRONT/goodvibing_app/src/EditStory.js
@@ -1,10 +1,3 @@
-import {
-  Button,
-  CssBaseline,
-  makeStyles,
-  TextareaAutosize,
-  TextField,
-} from "@material-ui/core";
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams, useHistory } from "react-router-dom";
@@ -42,71 +35,41 @@ function EditStory({ story }) {
       dispatch({ type: "SET_ERROR", message: data.message });
     }
   };
-  const useStyles = makeStyles((theme) => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      width: "100%",
-    },
-  }));
-  const classes = useStyles();
 
   return (
     <>
-      <div className="mi historia">
+      <div className="historia">
         <StoryUserEdit />
       </div>
       <form className="mystory" onSubmit={handleSubmit}>
-        <CssBaseline />
-        <div className={classes.paper}>
-          <p>
-            <label>
-              <TextField
-                id="standard-full-width"
-                label="Título"
-                middle
-                style={{ margin: 40 }}
-                placeholder="Escribe un título..."
-                margin="normal"
-                value={title}
-                onChange={(e) => settitle(e.target.value)}
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              <TextareaAutosize
-                aria-label="empty textarea"
-                style={{ margin: 40 }}
-                rowsMin={8}
-                placeholder="Escribe tu historia..."
-                value={body}
-                onChange={(e) => setBody(e.target.value)}
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              <TextField
-                id="standard-basic"
-                style={{ margin: 40 }}
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-                type="date"
-              />
-            </label>
-          </p>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Editar historia
-          </Button>{" "}
-        </div>
+        <p>
+          <label>
+            <input
+              placeholder="Escribe un título..."
+              value={title}
+              onChange={(e) => settitle(e.target.value)}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            <input
+              placeholder="Escribe tu historia..."
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            <input
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+            />
+          </label>
+        </p>
+        <button>Editar historia</button>
       </form>
     </>
   );

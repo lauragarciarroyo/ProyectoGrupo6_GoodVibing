66339f36e32abbbd8105f9a50a2841ddaf4ca44a
Laura García Arroyo <lauragarciarroyo@gmail.com>
2021-08-09 16:16:41 +0200

    materialUI

--- 1.-FRONT/goodvibing_app/src/MyStories.js
+++ 1.-FRONT/goodvibing_app/src/MyStories.js
@@ -1,7 +1,6 @@
 import UseFetchToken from "./useFetchToken";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { Button, makeStyles } from "@material-ui/core";
 import "./MyStories.css";
 
 function MyStories() {
